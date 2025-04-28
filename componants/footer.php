2f91c8b81def36a7b95ccb43a9c14beee796be60
karammaarouf <karamma3rouf@gmail.com>
2025-04-28 19:24:58 +0300

    add user home

---
+++ componants/footer.php
@@ -0,0 +1,23 @@
+<footer class="bg-dark text-light py-4 mt-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h5>About Us</h5>
+                <p>Your trusted online store for quality products.</p>
+            </div>
+            <div class="col-md-6">
+                <h5>Contact</h5>
+                <p>
+                    <i class="bi bi-envelope"></i> Email: karamma3rouf@gmail.com<br>
+                    <i class="bi bi-telephone"></i> Phone: +96394902486
+                </p>
+            </div>
+        </div>
+        <hr>
+        <div class="text-center">
+            <p>&copy; <?php echo date('Y'); ?> Store. All rights reserved.</p>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
---
+++ componants/header.php
@@ -0,0 +1,40 @@
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">Store</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="products.php">Products</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="categories.php">Categories</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="offers.php">Special Offers</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Contact Us</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="cart.php">
+                            <i class="bi bi-cart"></i> Cart
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="auth/login.php">
+                            <i class="bi bi-person"></i> Account
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
